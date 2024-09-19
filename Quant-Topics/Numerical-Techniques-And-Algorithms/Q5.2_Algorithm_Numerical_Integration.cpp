0c1ca4f15a09240f6b4caa8aaad6ba9ebede428e
jbucsa <justin.bucsa@gmail.com>
2024-09-19 11:49:23 -0500

    Fix the naming bug. May need to switch between the vector and array in my MyArray decorations.

--- Quant-Topics/Numerical-Techniques-And-Algorithms/Q5.2_Algorithm_Numerical_Integration.cpp
+++ Quant-Topics/Numerical-Techniques-And-Algorithms/Q5.2_Algorithm_Numerical_Integration.cpp
@@ -1,6 +1,18 @@
+#include <vector>
+
 // Suppose an asset takes values from a discrete set v_j and the probability of v_j is p_j. Use this algorithm to produce the random variable for this asset from a uniformly distributed random variable.
 
 //  We can present a solution by using a simple array class from xlw project but you could use any reasonable vector class.
+
+
+// Example using a type alias
+using MyArray = std::array<double>; 
+
+// Or, example defining a struct
+struct MyArray {
+    std::vector<double> data;
+    // ... other members if needed
+};
 
 double Simulate (const MyArray& p, const MyArray& v, double u)
 {
